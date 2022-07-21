2445d45712894966db1f9f92c57cfb192a9d4a7e
Kelvin Omozokpia <kelvinjune2@gmail.com>
2022-07-21 05:17:44 +0100

    updated project

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a5e1bd82474ac43cf81517fc848ea841-1550527265.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
